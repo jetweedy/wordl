9ad716bfe587b7ba5c0189729fdbcc3e73197617
Jon Tweedy <jetweedy@unc.edu>
2022-01-17 15:29:09 +0000

    m

--- public/main.js
+++ public/main.js
@@ -208,7 +208,7 @@ var startup = () => {
 	$("#keypad").on("keyup input", function(e) {
 		$("#keypad").css({"background-color":"yellow"});
 		handleKeyInput(e);
-		$("#keypad").val("");
+//		$("#keypad").val("");
 	});
 	$("#keypad").focus();
 
